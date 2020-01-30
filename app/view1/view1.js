36999d302cb6895e37b1131d33a877951033d09d
everson <eversonneris@hotmail.com>
2020-01-30 12:52:59 -0300

    add new implementations

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,24 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', [function($scope) {
+	
+	$scope.GenerateTable = function() {
+		
+		    $http.get('http://localhost:5000/pessoa-fisica/obter').
+        then(function(response) {
+            $scope.dataResponse = response.data;
+        });
+		
+		
+	};
+	
+			// let xhr = new XMLHttpRequest();
+			// xhr.open('GET', 'http://localhost:5000/pessoa-fisica/obter');
+			// $scope.dataResponse = [];
+			// xhr.onload = function ()
+			// {
+				// $scope.dataResponse = JSON.parse(xhr.responseText);
+			// }
+			// xhr.send();
 }]);
